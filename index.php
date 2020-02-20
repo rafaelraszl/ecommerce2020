bc0e8120a53dcda8c4421d4d312613489a2d85c8
Rafael Raszl <rafaelmsraszl@gmail.com>
2020-02-20 02:05:30 -0300

    Criado classe Page

--- index.php
+++ index.php
@@ -2,17 +2,19 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+use \Mekhet\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Mekhet\DB\Sql();
+	$page = new Page();
     
-    $results = $sql->select("SELECT * FROM tb_users");
-    
-    echo json_encode($results);
+    $page->setTpl("index");
+
 });
 
 $app->run();
