2c5e2638db3dbb01d884b8835a39515e40b6c70f
Rafael Raszl <rafaelmsraszl@gmail.com>
2020-02-20 19:33:30 -0300

    Template Admin

--- index.php
+++ index.php
@@ -4,14 +4,26 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Mekhet\Page;
+use \Mekhet\PageAdmin;
+
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
+// ROTA INDEX SITE
 $app->get('/', function() {
     
 	$page = new Page();
+    
+    $page->setTpl("index");
+
+});
+
+// ROTA INDEX ADMIN
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
     
     $page->setTpl("index");
 
--- vendor/mekhetbr/classes/src/Page.php
+++ vendor/mekhetbr/classes/src/Page.php
@@ -12,12 +12,13 @@ class Page {
         "data"=>[]
     ];
     
-    public function __construct($opts = array()){
+    public function __construct($opts = array(), $tpl_dir = "/views/")
+    {
         
         $this->options = array_merge($this->defaults, $opts);
         
         $config = array(
-            "tpl_dir"   => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "tpl_dir"   => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir" => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"     => false // set to false to improve the speed
            );
@@ -32,21 +33,24 @@ class Page {
         
     }
     
-    private function setData($data = array()) {
+    private function setData($data = array())
+    {
         
         foreach ($data as $key => $value) {
             $this->tpl->assign($key, $value);
         }
     }
     
-    public function setTpl($name, $data = array(), $returnHTML = false) {
+    public function setTpl($name, $data = array(), $returnHTML = false)
+    {
         
         $this->setData($data);
         
        return $this->tpl->draw($name, $returnHTML);
     }
     
-    public function __destruct(){
+    public function __destruct()
+    {
         
         $this->tpl->draw("footer");
         
